a145ddf7e6c86356124baab07313501319520582
Jhon Echavez <jhonfechavez@gmail.com>
2020-10-07 09:07:24 -0500

    Cambios en el front

---
+++ coin.php
@@ -0,0 +1,45 @@
+<?php 
+echo "hola";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--<link rel="stylesheet" href="./assets/css/coin.css">-->
+    <link rel="stylesheet" href="./assets/css/coin_style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <title>Head or tail</title>
+</head>
+<body class="coin-body-style" onload="TurnCoin()">    
+        <div class="coin-container">
+            <div class="coinDiv" id="coin">        
+            </div>            
+            <div class="awardDiv">
+                <h2 id="award" class="display">!</h2>        
+            </div>
+        </div>        
+        <div class="coin-div-disclaimer grid-system">
+            <div class="coin-disclaimer col-6">
+                <p>
+                    Condiciones y Restricciones <br>
+                    <br>
+                    Participan clientes con monto de compra desde 5 millones <br>
+                    Al completar sus datos, y dar click en el botón “Siguiente” Usted, autoriza de manera previa, expresa e informada a HENKEL COLOMBIANA S.A.S., con domicilio principal en Bogotá, D.C., con NIT 860.000.751-3 (“Henkel”), para que realice la recolección, almacenamiento, uso, circulación, supresión, transferencia, transmisión y, en general, cualquier operación o conjunto de operaciones en y sobre sus datos personales, entendidos como cualquier información vinculada o que pueda asociársele (los “Datos Personales”). Los Datos Personales incluyen, pero no se limitan a, datos de identificación, información de contacto e información sobre el uso de nuestros productos. La presente autorización se otorga para el cumplimiento de los fines de Henkel, que incluyen, pero no se limitan a, el otorgamiento de la garantía de productos; el cumplimiento de las políticas internas de Henkel, incluyendo la Política de Tratamiento de la Información de Henkel (la “Política”); la verificación del cumplimiento de obligaciones; ofrecer y promocionar productos nuevos y existentes; gestionar y consolidar registros; realizar campañas de actualización de datos; realizar estudios de seguridad, para la prevención de fraudes, lavado de activos y financiación del terrorismo, entre otros; realizar y organizar actividades promocionales; la administración de sus sistemas de información y comunicaciones; eventuales investigaciones con el fin de dar cumplimiento a los reglamentos internos; el reporte de información a las autoridades competentes, y la consolidación de bases de datos, así como las otras finalidades que sean informadas por medio de la Política. La presente autorización cubre la transferencia o transmisión de los Datos Personales a terceras personas que podrán estar domiciliadas en terceros países, incluso en países que no ofrezcan niveles adecuados de protección. Usted acepta y conoce que ha sido informado, de los derechos que le asisten en su calidad de titular de los Datos Personales, entre los que se encuentran el derecho a presentar a solicitudes de información, actualización, supresión y/o rectificación sobre los Datos Personales. Para conocer más sobre los derechos que le asisten, las formas para ejercerlos y las finalidades y tipos de tratamiento a los que los Datos Personales serán sometidos, lo invitamos a consultar la Política, disponible en www.henkel.com.co. Cualquier consulta, solicitud, reclamo, queja y petición relacionada con sus datos personales, podrá dirigirla a notificación.privacidad@henkel.com.
+
+                </p>
+            </div>
+        </div> 
+    <!--</div>-->    
+    <!--<div class="title">        
+        <h1>Para iniciar de click en la moneda!!</h1>
+    </div>
+    <div class="coinDiv" id="coin">        
+    </div>
+    <div class="awardDiv">
+        <h2 id="award" class="display">!</h2>        
+    </div>-->
+    <script src="./assets/scripts/coin.js"></script>    
+</body>
+</html>
